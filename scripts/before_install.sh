ab7807582fd6ea23394e808e0f2cb71eb2ef28b1
Shubham-Tiwar <shubham.tiwari28@gmail.com>
2024-11-13 14:03:20 +0530

    pipelineaws

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
 #_Change_Working_Directory
-cd /home/ec2-user/server
+cd /home/ubuntu/server
 
 #_Update_&_Set_Node_Version
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+#curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 
 #_Download_Node_&NPM
-yum -y install nodejs npm
+#yum -y install nodejs npm
 
 #_Download_PM2
-npm install pm2@latest -g
+#npm install pm2@latest -g
+#
+# Update the package list
+sudo apt update
+
+# Install the required dependencies for adding new repositories
+sudo apt install -y curl
+
+# Add the NodeSource repository for Node.js 14.x
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+
+# Install Node.js and npm
+sudo apt install -y nodejs
+
+# Install PM2 globally
+sudo npm install pm2@latest -g
+
